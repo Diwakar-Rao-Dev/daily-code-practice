8c8767cc86833a1dfc9b25ec282dc21541e6f489
Diwakar-Rao-Dev <rajputdiwakar001@gmail.com>
2026-09-20 01:08:40 +0530

    done

---
+++ javascript/index3.js
@@ -0,0 +1,5 @@
+let student = [
+    { name:"Shree Ram Ji", name:"Shree Hanumaan Ji", Homecity:"Ayodhya"},
+    { name:"Diwakar Rao", name:"Ashish Dixit", Homecity:"Ramkola"},
+    { name:"Aman", name:"Alok", Homecity:"xyz--"}
+]
