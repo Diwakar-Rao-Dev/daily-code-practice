22d9487fa6a66e602d75596214ce718195693d3a
Diwakar-Rao-Dev <rajputdiwakar001@gmail.com>
2026-09-22 23:11:57 +0530

    done

--- javascript/index6.js
+++ javascript/index6.js
@@ -1,25 +1,59 @@
-console.log("37" + 5);
+// console.log("37" + 5);
 
 
-console.log("50" - 25);
+// console.log("50" - 25);
 
 
-console.log("50" - 25);
+// console.log("50" - 25);
 
 
-console.log(Number("50") + 50);
+// console.log(Number("50") + 50);
 
 
-console.log(Number("25") - 15);
+// console.log(Number("25") - 15);
 
 
-console.log(Number("2") * 4);
+// console.log(Number("2") * 4);
 
 
-console.log(10 == 10);
+// console.log(10 == 10);
 
 
-console.log(10 === 10);
+// console.log(10 === 10);
 
 
-console.log(10 === 5);
+// console.log(10 === 5);
+
+
+// console.log(2**2);
+
+
+// console.log(5 && 5);
+
+
+
+// let a = 19
+// let b = "voterid"
+
+// if (a >= 16 && voterid ){
+//         console.log("eligible for vate");
+
+// } else {
+//         console.log( "not eligible for vote");
+
+// }
+
+
+
+let a = 18
+let b = "license"
+
+if (a >= 18 && licence) {
+    console.log("drive");
+} else {
+    console.log("not drive");
+}
+
+
+
+
