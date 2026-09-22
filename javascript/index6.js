ef120892c1145d17c0b86d090588cceaed102b4a
Diwakar-Rao-Dev <rajputdiwakar001@gmail.com>
2026-09-22 11:57:52 +0530

    done

--- javascript/index6.js
+++ javascript/index6.js
@@ -1,9 +1,25 @@
 console.log("37" + 5);
 
-console.log("50" - 25);
 
 console.log("50" - 25);
+
+
+console.log("50" - 25);
+
 
 console.log(Number("50") + 50);
 
+
 console.log(Number("25") - 15);
+
+
+console.log(Number("2") * 4);
+
+
+console.log(10 == 10);
+
+
+console.log(10 === 10);
+
+
+console.log(10 === 5);
