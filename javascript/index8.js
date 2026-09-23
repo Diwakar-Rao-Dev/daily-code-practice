b5f058b7ed9ffcfc6cdbb4238e71834539039ebf
Diwakar-Rao-Dev <rajputdiwakar001@gmail.com>
2026-09-24 00:20:22 +0530

    done

--- javascript/index8.js
+++ javascript/index8.js
@@ -1,4 +1,4 @@
-let day = 4
+/* let day = 4
 switch(day){
     case 1:
         console.log("monday");
@@ -23,9 +23,49 @@ switch(day){
         break;
 default:
 console.log("invalid day");
-}  
+} */  
 
 
-
+let month = 8
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("jun");
+        break;
+    case 7:
+        console.log("jul");
+        break;
+    case 8:
+        console.log("aug");
+        break;
+    case 9:
+        console.log("sep");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 11:
+        console.log("nov");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+default:
+console.log("invilid month");
+} 
 
 
