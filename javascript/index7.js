86367d1aade22e15dfe85e526eaf416b7db84f21
Diwakar-Rao-Dev <rajputdiwakar001@gmail.com>
2026-09-23 17:44:40 +0530

    done

--- javascript/index7.js
+++ javascript/index7.js
@@ -1,5 +1,8 @@
-let a =  15
-let b = 22
-let data =(a > b)? ("greater"):("not greater")
-console.log(data);
+// let a =  15
+// let b = 22
+// let data =(a > b)? ("greater"):("not greater")
+// console.log(data);
 
+let student = 45
+let data = (student >=40)? ("passed"):("failed")
+console.log(data);
